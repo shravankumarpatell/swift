817b9e3610019c9e5453e9c22946b68b12cbb856
shravankumarpatell <shravankumarpatelofficial@gmail.com>
2025-07-16 23:25:32 +0530

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '.build',    // <— now Vite will write to ./frontend/.build
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
