4be48ed801a7bd5a4b01f4a90579938b960aed5c
shravankumarpatell <shravankumarpatelofficial@gmail.com>
2026-02-12 23:34:09 +0530

    server_updated

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://swift-server-bn1o.onrender.com"
+export const BACKEND_URL = "https://swift-server1.onrender.com"
 // export const BACKEND_URL = "http://localhost:3000"
